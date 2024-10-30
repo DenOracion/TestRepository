3758ed31e01a8a8d7f37e3d3f3b161edfb882cb5
Denson Oracion <miguel.oracion@outlook.com>
2024-10-30 14:05:17 +0400

    examples of lists

---
+++ lists.py
@@ -0,0 +1,23 @@
+marks = [10, 20, 30 ,40, 50, 60, 70, 80, 90, 100]
+print(marks)
+
+#list number 2 
+even_numbers = [2, 4, 6, 8, 10]
+print(even_numbers)
+
+#list number 3
+names = ["Molly", "Steven", "Will", "Alicia", "Adriana"]
+print(names)
+
+#list number 4 
+info = ["Alicia", 27, 1550.87]
+print(info) 
+
+#list concatenation
+list1 = [2, 45, 67, 89, 25]
+list2 = [2, 45, 677, 81, 25]
+print(list1 + list2)
+
+#list slicing
+list3 = [11, 25, 21, 30, 30, 27, 18, 22]
+print(list3[0:5])
